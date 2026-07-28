9c433f06d48e54bb84202414f2ca68e7d578f173
Mannat sharma <sharmamannat474@gmail.com>
2026-07-28 22:45:27 +0530

    Initial commit

---
+++ bloodlink-backend/src/main/java/com/bloodlink/bloodlink_backend/BloodlinkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bloodlink.bloodlink_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodlinkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodlinkBackendApplication.class, args);
+	}
+
+}
---
+++ bloodlink-backend/src/test/java/com/bloodlink/bloodlink_backend/BloodlinkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloodlink.bloodlink_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodlinkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
